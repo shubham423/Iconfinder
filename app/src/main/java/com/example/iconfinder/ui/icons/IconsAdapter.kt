cb311689bf64592bd439a5e1f5abd10576869b9e
shubham423 <shubh991172@gmail.com>
2022-10-20 14:52:31 +0530

    icon item ui fix

--- app/src/main/java/com/example/iconfinder/ui/icons/IconsAdapter.kt
+++ app/src/main/java/com/example/iconfinder/ui/icons/IconsAdapter.kt
@@ -1,11 +1,13 @@
 package com.example.iconfinder.ui.icons
 
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
+import com.example.iconfinder.R
 import com.example.iconfinder.databinding.ItemIconBinding
 import com.example.iconfinder.models.Icon
 
@@ -23,7 +25,13 @@ class IconsAdapter : ListAdapter<Icon, IconsAdapter.HomeViewHolder> (
 {
     inner class HomeViewHolder(private val binding:ItemIconBinding): RecyclerView.ViewHolder(binding.root) {
         fun bind(icon: Icon, position: Int) {
-            binding.pricingTv.text=if (icon.isPremium) "Paid" else "Free"
+           if (icon.isPremium){
+               binding.pricingImg.setImageResource(R.drawable.premium_icon)
+           }else{
+               binding.downloadIcon.visibility=View.VISIBLE
+               binding.pricingImg.setImageResource(R.drawable.free_icon)
+           }
+            binding.nameTv.text=icon.tags[0]
          Glide.with(itemView)
              .load(icon.rasterSizes[icon.rasterSizes.lastIndex].formats[0].previewUrl)
              .into(binding.iconImg)
